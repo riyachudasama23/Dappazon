9db46c35264ff11fc993ac6898a5c4a9f6b8bf4f
Riya <riyachudasama1123@gmail.com>
2024-08-28 22:22:15 +0530

    metamask connection

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import './App.css';
 import { useEffect, useState } from 'react';
-import { ethers } from 'ethers';
+//import { ethers } from 'ethers';
+import {ethers} from 'hardhat';
 import Navigation from './components/Navigation';
 
 function App() {
 
   const [account, setAccount] = useState(null)
+  const [provider, setProvider] = useState(null)
+
 
   const loadBlockchainData = async() => {
+    //connect to bc
+    const provider = new ethers.providers.Web3Provider(window.ethereum) //takes our metamask connection
+    setProvider(provider)
+
+    // const network = await provider.getNetwork()
+    // console.log(network)
+
     
+    //connect to smart contract
+
+    //load products
     console.log("loading...")
   }
 
   useEffect(() => {
-    loadBlockchainData()
+    loadBlockchainData(
+      
+    )
   }, [])
 
   return (
     <div className="App">
       <Navigation account= {account} setAccount={setAccount} />
       <h2>
-        Welcome to Dappazon
+        Dappazon Best Sellers
       </h2>
     </div>
   );
