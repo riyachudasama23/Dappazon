d41a9a4dd8cdca018f85afd68a9ed94eb1d8a9d5
Riya <riyachudasama1123@gmail.com>
2024-08-31 11:24:21 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { ethers } from 'ethers';
 import { BrowserProvider } from 'ethers';
 import Navigation from './components/Navigation';
 import Section from './components/Section';
+import Product from './components/Product';
 
 //ABIs
 import Dappazon from './abis/Dappazon.json';
@@ -12,12 +13,18 @@ function App() {
 
   const [account, setAccount] = useState(null)
   const [provider, setProvider] = useState(null)
+  const [dappazon, setDappazon] = useState(null)
+  
   const [electronics, setElectronics] = useState(null)
   const [clothing, setClothing] = useState(null)
   const [toys, setToys] = useState(null)
 
-  const togglePop = () => {
-    console.log("togglePop");
+  const [item, setItem] = useState({})
+  const [toggle, setToggle] = useState(false)
+
+  const togglePop = (item) => {
+    setItem(item)  
+    toggle ? setToggle(false) : setToggle(true)
   }
 
   const loadBlockchainData = async() => {
@@ -37,6 +44,7 @@ function App() {
       Dappazon , 
       provider
     )
+    setDappazon(dappazon)
 
     //load products
     const items = []
@@ -58,9 +66,7 @@ function App() {
   }
 
   useEffect(() => {
-    loadBlockchainData(
-      
-    )
+    loadBlockchainData()
   }, [])
 
   return (
@@ -76,6 +82,10 @@ function App() {
           <Section title={"Electronics & Gadgets"} items={electronics} togglePop={togglePop}></Section>
           <Section title={"Toys & Gaming"} items={toys} togglePop={togglePop}></Section>
         </>
+      )}
+
+      {toggle && (
+        <Product item={item} provider={provider} account={account} dappazon={dappazon} togglePop={togglePop} />
       )}
     </div>
   );
